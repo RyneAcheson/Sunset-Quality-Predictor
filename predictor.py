9742e1b8cdb2b62daf7972d73953b278f26f1039
RyneAcheson <109265450+RyneAcheson@users.noreply.github.com>
2024-12-09 13:47:23 -0500

    Update predictor.py
    API data retrieval

--- predictor.py
+++ predictor.py
@@ -9,6 +9,59 @@ import pyinputplus as pyip
 # Date Started: June 26th 2023
 # Date Finished:
 
+def getData(target_date, zip_code):
+
+    # Declare and Intialize Important Variables
+    
+    name = ""
+    latitude = ""
+    longitude = ""
+    sunset_time = ""
+    sunset_hour = 0
+    unix_sunset_hour = 0
+    humidity = 0
+    aqi_data = 0
+    cloud_cover = 0
+    cloud_height = 0
+    cloud_data = []
+    surface_temperature_f = 0
+    dew_point_f = 0
+
+    # Translate the zipcode into an approximate longitude and latitude
+
+    geocoding_url = f"OpenWeatherMap API URL"
+    response = requests.get(geocoding_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        name = data["name"]
+        latitude = data["lat"]
+        longitude = data["lon"]
+
+    else:
+        print("Error occurred while fetching weather data. ")
+        print(response.status_code)
+        quit()
+    
+    if target_date == "Today":
+        day = date.today()
+    elif target_date == "Tomorrow":
+        day = date.today() + timedelta(1)
+
+    weather_url = f"WEATHER API"
+    response = requests.get(weather_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        sunset_time = data["astronomy"]["astro"]["sunset"]
+        print(sunset_time)
+    
+    else:
+        print("Error occurred while fetching weather data. ")
+        print(response.status_code)
+        quit()
+    
+    return
 
 def validate_zip_code(zip_code):
     # Checks that the zip code entered is a 5-digit number, otherwise will raise an exception
