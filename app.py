8759c3e775971c1bd558185ae52b36338e3eb095
RyneAcheson <109265450+RyneAcheson@users.noreply.github.com>
2024-12-12 15:24:20 -0500

    zip code information

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 if not WEATHER_API_KEY:
     raise ValueError("No WEATHER_API_KEY set for Flask application")
 
+ZIP_CODE_API_KEY = os.getenv("ZIP_CODE_API_KEY")
+if not ZIP_CODE_API_KEY:
+    raise ValueError("No ZIP_CODE_API_KEY set for Flask application")
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
@@ -22,10 +26,15 @@ def index():
 @app.route("/check_sunset", methods=["POST"])
 def check_sunset():
     zipcode = request.form.get("zipcode")
-    if not zipcode or not zipcode.isdecimal() or len(zipcode) != 5 or not valid_zip(zipcode):
+    if not zipcode or not zipcode.isdecimal() or len(zipcode) != 5:
         return render_template("index.html", error="Please enter a valid ZIP Code")
     
-    latitude, longitude = geocode_zip(zipcode)
+    zip_code_info = geocode_zip(zipcode)
+    latitude = zip_code_info["latitude"]
+    longitude = zip_code_info["longitude"]
+    city = zip_code_info["city"]
+    state = zip_code_info["state"]
+
     if latitude is None or longitude is None:
         return render_template("index.html", error="Invalid ZIP Code Provided")
     
@@ -35,26 +44,48 @@ def check_sunset():
 
     return render_template("index.html",
                            score=score,
+                           latitude=latitude,
+                           longitude=longitude,
                            location_type=location_type,
+                           city=city,
+                           state=state,
                            zipcode=zipcode)
 
-def geocode_zip(zip_code, country_code="US"):
-
-    url = f"http://api.openweathermap.org/geo/1.0/zip"
+def geocode_zip(zip_code, country_code="us"):
+    print(zip_code)
+    url = "https://api.zipcodestack.com/v1/search"
     params = {
-        "zip": f"{zip_code}, {country_code}",
-        "appid": WEATHER_API_KEY
+        "codes" : zip_code,
+        "country" : country_code
     }
-    
-    response = requests.get(url, params=params, timeout=10)
+    headers = {
+        "apikey": ZIP_CODE_API_KEY
+    }
+
+    response = requests.request('GET', url, headers=headers, params=params)
     if response.status_code == 200:
         data = response.json()
-        latitude = data.get("lat")
-        longitude = data.get("lon")
-        return latitude, longitude
+        print(data)
+        if data["results"] == []:
+            return None, None
+        latitude = data["results"][zip_code][0]["latitude"]
+        longitude = data["results"][zip_code][0]["longitude"]
+        city = data["results"][zip_code][0]["city"]
+        state = data["results"][zip_code][0]["state"]
+
+        info = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "city": city,
+            "state": state
+        }
+
+        return info
     else:
         print(f"Geocoding failed for ZIP: {zip_code}. Status Code: {response.status_code}")
-        
+
+    return None, None
+
 def get_weather_data(latitude, longitude):
     return {
         "cloud_cover": 0,
